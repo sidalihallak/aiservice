7649af66db8865aca21c9f6cf261ca3e7f4c3935
sidalihallak <sid.hallak@gmail.com>
2023-11-30 17:30:44 +0100

    add service

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask import request
+import g4f
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    g4f.debug.logging = True  # Enable logging
+    g4f.check_version = False  # Disable automatic version checking
+    content = request.args.get('content')
+    print("content", content)
+    response = g4f.ChatCompletion.create(
+        model=g4f.models.gpt_35_turbo,
+        provider=g4f.Provider.AiChatOnline,
+        messages=[{"role": "user", "content": content}],
+    )  # Alternative model setting
+    print("response", response)
+
+    return jsonify(
+        response=response
+    )
+
